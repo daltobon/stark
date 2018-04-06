ed08a8d30de568ee02f0abb9dd00fe20e656d003
daltobon <daniel18.tobon@gmail.com>
2018-04-06 18:53:45 -0500

    add files

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   root 'questions#index'
   resources :questions do
     resources :answers
-  end
+end
+    resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
